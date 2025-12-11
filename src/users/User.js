e48823e504fb20f46bdb85d0dfa17b0cac580c1a
Albert Souza <albert.souza@inovafarma.com.br>
2025-12-11 16:57:24 -0300

    refactor: change way to encrypt user password

--- src/users/User.js
+++ src/users/User.js
@@ -1,32 +1,19 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    username: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        select: false
-    },
-    avatar: {
-        type: String,
-        required: true
-    },
-    background: {
-        type: String,
-        required: true
-    }
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true, select: false },
+    avatar: { type: String, required: true },
+    background: { type: String, required: true },
 });
+
+UserSchema.pre("save", async function(next){
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+    next();
+})
 
 export const User = mongoose.model("User", UserSchema);
--- src/users/user.service.js
+++ src/users/user.service.js
@@ -3,13 +3,5 @@ import bycrypt from 'bcrypt';
 
 export const findAll = async () => await User.find();
 export const findById = async (id) => await User.findById(id);
-
-export const create = async (body) => {
-    const saltRounds = 10;
-    const hashedPassword = await bycrypt.hash(body.password, saltRounds);
-
-    body.password = hashedPassword;
-    return await User.create(body);
-}
-
+export const create = async (body) => await User.create(body);
 export const update = async (id, body) => await User.findByIdAndUpdate(id, body, { new: true });
