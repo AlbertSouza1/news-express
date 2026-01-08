de7ed26d6befe5a1c40167d9ffe0bbb674910dce
Albert Souza <albert.neves.souza@gmail.com>
2026-01-08 14:46:32 -0300

    feat: add cors to fix communication with frontend

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import userRoute from './src/users/user.route.js';
 import authRoute from './src/auth/auth.route.js';
 import newsRoute from './src/news/news.route.js';
@@ -11,6 +12,13 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 connectDatabase();
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 
